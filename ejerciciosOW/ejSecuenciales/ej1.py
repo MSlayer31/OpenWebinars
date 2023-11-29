0c82dcb72a2329c335cd54e0edfaebcdb7aec80e
Manel Sebastián Álvarez <79845247+MSlayer31@users.noreply.github.com>
2023-11-29 21:32:15 +0100

    Delete ejerciciosOW/ejSecuenciales directory

--- ejerciciosOW/ejSecuenciales/ej1.py
+++
@@ -1,5 +0,0 @@
-
-print("Hola Mundo")
-
-name = input("¿Cómo se llama? \n")
-print("Me alegro de conocerle, " + name)
--- ejerciciosOW/ejSecuenciales/ej5.py
+++
@@ -1,8 +0,0 @@
-#Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius.
-#Recordar que la fórmula para la conversión es:
-
-#   C = (F-32)*5/9
-
-F = float(input("Grados Fahrenheit:\n"))
-C = (F-32)*5/9
-print("Grados Fahrenheit:",F,".\nGrados Celsius:",C)
